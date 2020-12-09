71d13e9be54461755e891cc139368ab13c2a5ce0
tatsuya.orikasa <peter-porker.ham@yahoo.ne.jp>
2020-12-09 15:20:13 +0000

    like-unlike

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,19 @@
 class ApplicationController < ActionController::Base
     
-    include SessionsHelper
+  include SessionsHelper
     
-    private
+  private
     
-    def require_user_logged_in
-        unless logged_in?
-          redirect_to login_url
-        end
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
     end
+  end
     
-    def counts(user)
-        @count_microposts = user.microposts.count
-        @count_followings = user.followings.count
-        @count_followers = user.followers.count
-    end
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+    @count_likes = user.likes.count
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc).page(params[:page]).per(10)
   end
 
   def show
@@ -35,14 +35,19 @@ class UsersController < ApplicationController
   
   def followers
     @user = User.find(params[:id])
-    @followers = @user.followers.pagae(params[:page])
+    @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likes.page(params[:page])
+    counts(@user)
+  end
+  
   private
 
   def user_params
-  
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
